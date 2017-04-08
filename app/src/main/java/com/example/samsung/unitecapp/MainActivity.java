1603cdd52c5740a2657ea349671f883c58949cfa
Chen <timeiseth@gmail.com>
2017-04-08 14:48:33 +1200

    create management

---
+++ app/src/main/java/com/example/samsung/unitecapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.samsung.unitecapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showFlipper(ViewFlipper image){
+
+
+    }
+
+
+}
