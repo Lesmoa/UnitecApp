5d6e0f538ba7874094a9cc89cf6dbb93a526871b
Chen <timeiseth@gmail.com>
2017-04-09 23:24:22 +1200

    webview finished

---
+++ app/src/main/java/com/example/samsung/unitecapp/CareerAndStudyOptionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.samsung.unitecapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CareerAndStudyOptionsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_careerandstudyoptions);
+    }
+}
